0e73d6b1bf025c4a1fb8b072046292aba1178e2f
Aly60 <alyashmawy98@gmail.com>
2024-08-11 12:52:04 +0300

    comment

--- src/Login.jsx
+++ src/Login.jsx
@@ -208,15 +208,6 @@ const Login = () => {
               </div>
             </form>
           )}
-          <div className="mt-4 flex items-center w-full text-center">
-            <a
-              href="/"
-              className="text-xs text-gray-500 capitalize text-center w-full"
-            >
-              Don&apos;t have an account yet?
-              <span className="text-blue-700"> Sign Up</span>
-            </a>
-          </div>
         </div>
       </div>
     </div>
